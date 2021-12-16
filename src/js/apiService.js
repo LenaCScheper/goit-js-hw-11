49b6fce27689cea53ee374fc3498f1a032f4df7a
LenaCScheper <ven.lenysja@ukr.net>
2021-12-16 11:14:52 +0200

    изменения

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,43 @@
-export default async(name, page) => {
+import axios from 'axios';
+const API_KEY = '24794721-8b40f8604d43363d1fa3375eb';
+const BASE_URL = `https://pixabay.com/api/`;
+
+export default class PicturesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async fetchPictures() {
+    const searchParametrs = {
+      params: {
+        key: API_KEY,
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        per_page: this.per_page,
+      },
+    };
+
     try {
-        const picturesList = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&page=${page}&per_page=9&key=24794721-8b40f8604d43363d1fa3375eb`);
-        return picturesList.json();
+      const response = await axios.get(BASE_URL, searchParametrs);
+
+      const dataReceived = response.data;
+      // console.log('DATA RECEIVED Fetchpictures Funct: ', dataReceived);
+      this.incrementPage();
+      return dataReceived;
     } catch (error) {
-        return error;
+      console.error(error);
     }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
 }
